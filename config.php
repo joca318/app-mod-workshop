3407d5f0f68cdc7162527823d64f3d65d6c11d5c
Jose Neto <joca318@gmail.com>
2025-04-11 13:36:15 +0000

    "adding cloudbuild.yaml"

--- config.php
+++ config.php
@@ -1,9 +1,17 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
-$db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+#$db_host = '35.184.102.226';
+#$db_name = 'image_catalog';
+#$db_user = 'root';
+#$db_pass = 'admin123';
+
+// Database configuration with ENV variables. Set default values as well 
+$db_host = getenv('DB_HOST') ?: '35.184.102.226';
+$db_name = getenv('DB_NAME') ?: 'image_catalog';
+$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
+$db_pass = getenv('DB_PASS') ?: 'Access#123';
+
+
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
